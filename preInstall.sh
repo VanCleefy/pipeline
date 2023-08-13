59ab177ac33a9c8226379544eafed21aa2b97bfd
VanCleefy <vancleefy@protonmail.com>
2023-08-13 18:22:06 +0300

    First commit

---
+++ preInstall.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Install necessary packages/tools
+sudo apt-get update
+sudo apt-get install -y python3 python3-pip docker.io
+
+# Create a virtual environment and activate it
+python3 -m venv venv
+source venv/bin/activate
+
+# Install Python packages
+pip install flask
+
+echo "Tools and environments installed successfully."
