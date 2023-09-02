df21b5d1dbfa9e4677c50434ec80e00e76bdbbb5
VanCleefy <105280944+VanCleefy@users.noreply.github.com>
2023-09-02 22:57:34 +0300

    Update preInstall.sh

--- preInstall.sh
+++ preInstall.sh
@@ -5,8 +5,8 @@ sudo apt-get update
 sudo apt-get install -y python3 python3-pip docker.io
 
 # Create a virtual environment and activate it
-python3 -m venv flask-app/venv
-source flask-app/venv/bin/activate
+python3 -m venv venv
+source venv/bin/activate
 
 # Install Python packages
 pip install flask
