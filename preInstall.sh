dbdc5415ad516aecb9b8abe4f7607e50c00a0564
VanCleefy <105280944+VanCleefy@users.noreply.github.com>
2023-09-02 21:48:39 +0300

    Update preInstall.sh
    changed environment location to flask-app

--- preInstall.sh
+++ preInstall.sh
@@ -5,7 +5,7 @@ sudo apt-get update
 sudo apt-get install -y python3 python3-pip docker.io
 
 # Create a virtual environment and activate it
-python3 -m venv venv
+python3 -m venv flask-app/venv
 source venv/bin/activate
 
 # Install Python packages
